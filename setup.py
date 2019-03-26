e931721aa480b9842377e8cd18fd647ce6c35569
Damir Cavar <dcavar@me.com>
2019-03-26 17:42:56 -0400

    Initial module content.

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nltkjsonnlp",
+    version="0.0.1",
+    author="Damir Cavar, Oren Baldinger, Maanvitha Gongalla, Anurag Kumar, Murali Kammili",
+    author_email="damir@cavar.me",
+    description="The Python NLTK JSON-NLP package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcavar/NLTK-JSON-NLP",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'nltk>=3.4',
+        'pyjsonnlp>=0.2.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
